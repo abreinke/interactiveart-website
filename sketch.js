2c3ec6ba35a925ffb8f4e5ba1729fafac42fc080
abreinke <42782067+abreinke@users.noreply.github.com>
2019-11-04 07:26:09 -0800

    changes

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+	// make canvas full screen
+	createCanvas(windowWidth, windowHeight);
+
+}
+for (let i=0; i<100; i++){
+
+}
+
+function draw() {
+size= +100
+
+	ellipse(mouseX, mouseY, 100);
+}
+function createCheckboxes(){
+	let checkboxes=[];
+	let size= 100;
+}
